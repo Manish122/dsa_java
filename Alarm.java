bfd6f6658c74f7fedc98a6bc945b6f2166708507
Manish <waliamanish122@gmail.com>
2022-11-22 20:44:20 +0530

    2nd

--- Alarm.java
+++
@@ -1,9 +0,0 @@
-import java.time.*; // import the LocalDate class
-
-public class Alarm {
-    public static void main(String[] args) {
-        // System.out.println("hello");
-        LocalTime myObj = LocalTime.now(); // Create a date object
-        System.out.println(myObj.toString().substring(0, 5) == "15:32"); // Display the current date
-    }
-}
